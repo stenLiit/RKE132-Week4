73bc048e505046216e0bac808031bccca682f483
stenliit <sten.liit@tktk.ee>
2024-09-30 12:12:24 +0300

    add week 4 code

---
+++ FoorLoopSum/Program.cs
@@ -0,0 +1,10 @@
+﻿int sum = 0;
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"i = {i}");
+    Console.WriteLine($"Current total: {sum}");
+    sum = sum + i;
+}
+
+Console.WriteLine($"Final total: {sum}");
